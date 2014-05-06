457ba9771eeb00004b7405e174e537c2c920dad4
marcohern <marcohern@gmail.com>
2014-05-06 17:35:45 -0500

    added new views

--- android/iWasteCash/gen/com/rocketteam/iwastecash/R.java
+++ android/iWasteCash/gen/com/rocketteam/iwastecash/R.java
@@ -995,7 +995,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050040;
+        public static final int action_settings=0x7f050042;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1023,9 +1023,11 @@ containing a value of this type.
         public static final int normal=0x7f050000;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
-        public static final int purchase_list_item_image=0x7f05003f;
-        public static final int purchase_list_item_subtitle=0x7f05003e;
-        public static final int purchase_list_item_title=0x7f05003d;
+        public static final int purchase_list=0x7f05003d;
+        public static final int purchase_list_item=0x7f05003e;
+        public static final int purchase_list_item_image=0x7f050041;
+        public static final int purchase_list_item_subtitle=0x7f050040;
+        public static final int purchase_list_item_title=0x7f05003f;
         public static final int radio=0x7f05002f;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
@@ -1102,8 +1104,9 @@ containing a value of this type.
         public static final int activity_iwaste_cash=0x7f030017;
         public static final int fragment_iwaste_cash=0x7f030018;
         public static final int fragment_purchase_balance=0x7f030019;
-        public static final int fragment_purchase_list_item=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int fragment_purchase_list=0x7f03001a;
+        public static final int fragment_purchase_list_item=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
     }
     public static final class menu {
         public static final int iwaste_cash=0x7f0c0000;

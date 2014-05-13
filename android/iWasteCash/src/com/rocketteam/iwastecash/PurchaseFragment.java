6058d453f89d206d642a6c8b64b71654d3d0b8e3
Marco Hernandez <marcohern@gmail.com>
2014-05-13 00:22:46 -0500

    added Ourchase fragment

--- android/iWasteCash/src/com/rocketteam/iwastecash/PurchaseFragment.java
+++ android/iWasteCash/src/com/rocketteam/iwastecash/PurchaseFragment.java
@@ -8,6 +8,10 @@ import android.view.ViewGroup;
 
 public class PurchaseFragment extends Fragment {
 	
+	public static PurchaseFragment newFragment() {
+		return null;
+	}
+	
 	public PurchaseFragment() {
 		
 	}
@@ -16,7 +20,7 @@ public class PurchaseFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		View v = inflater.inflate(R.layout.fragment_purchase_list, container);
+		View v = inflater.inflate(R.layout.fragment_purchase_list, container, false);
 		return v;
 	}
 }
